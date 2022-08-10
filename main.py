429b9fac8e5345f52590dcb07d2771d7d4f74d56
Hayden Huy <hayden.huyp@gmail.com>
2022-08-10 10:19:50 +0700

    Add menu option and file log feature

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 
 letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
@@ -6,42 +7,85 @@ letters = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
            'z')
 symbols = ('@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
            '*', '(', ')', '<')
-isPasswordLengthValid = False
 
-while not isPasswordLengthValid:
+
+def append_to_log(log_string):
+    password_log = open("password_log.txt", "a")
+    password_log.write(log_string + "\t" + str(datetime.datetime.today()) + "\n")
+    password_log.close()
+
+
+print("Menu Option: ")
+print("\tPress 1 to Generate new password")
+print("\tPress 2 to View recently generated password")
+
+is_option_valid = False
+while not is_option_valid:
     try:
-        password_length = int(input("Password Length: "))
-        isPasswordLengthValid = True
+        option = int(input("Please enter 1 or 2: "))
+        if option < 0 or option > 2:
+            raise Exception("Option must be either 1 or 2. Please enter again!")
+        is_option_valid = True
     except ValueError:
         print("Only digits are allowed! Please enter again!")
-        isPasswordLengthValid = False
+        is_option_valid = False
+    except:
+        is_option_valid = False
 
-# Strong Password must-have: letters, digits, symbols
+if option == 1:
+    is_password_length_valid = False
+    while not is_password_length_valid:
+        try:
+            password_length = int(input("Password Length: "))
+            if password_length < 4:
+                raise Exception("Password length must be greater than 3. Please enter again!")
+            is_password_length_valid = True
+        except ValueError:
+            print("Only digits are allowed! Please enter again!")
+            is_password_length_valid = False
+        except:
+            is_password_length_valid = False
 
-random_number = random.randint(0, len(letters) - 1)
-password = letters[random_number]
-random_number = random.randint(0, 9)
-password += str(random_number)
-random_number = random.randint(0, len(symbols) - 1)
-password += symbols[random_number]
+    random_number = random.randint(0, len(letters) - 1)
+    password = letters[random_number]
+    random_number = random.randint(0, 9)
+    password += str(random_number)
+    random_number = random.randint(0, len(symbols) - 1)
+    password += symbols[random_number]
 
-for i in range(4, password_length + 1):
-    random_number = random.randint(1, 10000)
+    for i in range(4, password_length + 1):
+        random_number = random.randint(1, 10000)
 
-    # uppercase letter
-    if random_number % 4 == 0:
-        password += letters[random.randint(0, len(letters) - 1)].upper()
-    # lowercase letter
-    elif random_number % 4 == 1:
-        password += letters[random.randint(0, len(letters) - 1)]
-    # digits
-    elif random_number % 4 == 2:
-        password += str(random.randint(0, 9))
-    # symbols
-    elif random_number % 4 == 3:
-        password += symbols[random.randint(0, len(symbols) - 1)]
+        # uppercase letter
+        if random_number % 4 == 0:
+            password += letters[random.randint(0, len(letters) - 1)].upper()
+        # lowercase letter
+        elif random_number % 4 == 1:
+            password += letters[random.randint(0, len(letters) - 1)]
+        # digits
+        elif random_number % 4 == 2:
+            password += str(random.randint(0, 9))
+        # symbols
+        elif random_number % 4 == 3:
+            password += symbols[random.randint(0, len(symbols) - 1)]
 
-print("Your generated password: " + password)
+    print("Your generated password: " + password)
+    append_to_log(password)
+
+elif option == 2:
+    try:
+        password_log = open("password_log.txt", "r")
+    except FileNotFoundError:
+        print("You have not generated any passwords yet. Try create one!")
+
+    print("Password \t Created on")
+    for password in password_log.readlines():
+        print(password)
+    password_log.close()
+
+
+
+
 
 
 
